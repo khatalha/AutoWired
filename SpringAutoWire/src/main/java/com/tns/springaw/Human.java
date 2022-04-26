65c517aafc3fddd7ea69f848d92d9cce8b924d2c
khatalha <khatalha2015@gmail.com>
2022-04-26 17:52:27 +0530

    First commit

---
+++ SpringAutoWire/src/main/java/com/tns/springaw/Human.java
@@ -0,0 +1,28 @@
+package com.tns.springaw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Human {
+	
+	public Human() 
+	{
+	}
+
+	Heart heart;
+	
+	
+    @Autowired
+    @Qualifier("octopusHeart")
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+
+
+	public void startPumping()
+	{
+		heart.pump();
+		
+	}
+
+}
